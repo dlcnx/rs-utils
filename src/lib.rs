d9db5c65b2938deea6ae3ce183111190b9401461
dlcn <microccl@foxmail.com>
2023-03-22 21:24:43 +0800

    change tests struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
-mod tests;
 mod lifo_queue;
 mod thread_pool;
 
 // pub use
 pub use lifo_queue::LifoQueue;
 pub use thread_pool::ThreadPool;
+
+#[cfg(test)]
+mod tests;
--- src/tests.rs
+++ src/tests.rs
@@ -1,27 +1,24 @@
-#[cfg(test)]
-mod test {
-    use crate::LifoQueue;
-    use crate::ThreadPool;
+use crate::LifoQueue;
+use crate::ThreadPool;
 
-    #[test]
-    fn lifo_queue() {
-        let mut q = LifoQueue::new();
-        q.push(1);
-        q.push(2);
-        assert_eq!(false, q.empty());
-        assert_eq!(2, q.len());
-        assert_eq!(2, q.pop().unwrap());
-        assert_eq!(1, q.pop().unwrap());
-        assert_eq!(true, q.empty());
-    }
+#[test]
+fn lifo_queue() {
+    let mut q = LifoQueue::new();
+    q.push(1);
+    q.push(2);
+    assert_eq!(false, q.empty());
+    assert_eq!(2, q.len());
+    assert_eq!(2, q.pop().unwrap());
+    assert_eq!(1, q.pop().unwrap());
+    assert_eq!(true, q.empty());
+}
 
-    #[test]
-    fn thread_pool() {
-        let pool = ThreadPool::new(2);
-        pool.execute(|| println!("1"));
-        pool.execute(|| println!("2"));
-        pool.execute(|| println!("3"));
-        pool.execute(|| println!("4"));
-        pool.execute(|| println!("5"));
-    }
+#[test]
+fn thread_pool() {
+    let pool = ThreadPool::new(2);
+    pool.execute(|| println!("1"));
+    pool.execute(|| println!("2"));
+    pool.execute(|| println!("3"));
+    pool.execute(|| println!("4"));
+    pool.execute(|| println!("5"));
 }
